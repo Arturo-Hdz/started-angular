e917f16fb8f729384c321686008be0e485ca32c4
baruch <lb@LBs-MacBook-Air.home>
2022-01-16 00:26:20 -0500

    Mapping Urls with Router Module #7

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+import { RouterModule, Routes } from '@angular/router';
+import { appRoutes } from './app.routing';
+
 import { AppComponent } from './app.component';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +19,11 @@ import { VideoDetailComponent } from './video-detail/video-detail.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes
+  ),
+    // HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ client/src/app/app.routing.ts
@@ -0,0 +1,32 @@
+import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+
+import { VideoListComponent } from "./video-list/video-list.component";
+import { VideoDetailComponent } from "./video-detail/video-detail.component";
+
+export const appRoutes: Routes = [
+    {
+        path:'',
+        component: VideoListComponent,
+    },
+    {
+        path:'videos',
+        component: VideoListComponent,
+    },
+    {
+        path:'videos/:slug',
+        component: VideoDetailComponent,
+    }
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            appRoutes
+        )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{}
