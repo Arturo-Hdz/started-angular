66d0ceede46efbc779cb0d4f7529378cf327b0e9
baruch <lb@LBs-MacBook-Air.home>
2022-02-04 02:07:39 -0500

    Http $ Featured #16

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,23 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   // prevented = false;
+  private req: any;
   homeImageList = [
     {image: "assets/images/nature/4.jpg", title:"image 4", link:'/videos/video-1'},
     {image: "assets/images/nature/5.jpg", title:"image 5", link:'/videos/video-1'},
     {image: "assets/images/nature/6.jpg", title:"image 6", link:'/videos/video-1'},
     {image: "assets/images/nature/4.jpg", title:"image 4", link:'/videos/video-1'}
-
   ]
-  constructor(private router:Router) { }
+  homeImageList2 = [
+    {image: "assets/images/nature/4.jpg", name:"image 4", slug:'/videos/video-1'},
+    {image: "assets/images/nature/5.jpg", name:"image 5", slug:'/videos/video-2'},
+    {image: "assets/images/nature/6.jpg", name:"image 6", slug:'/videos/video-3'},
+  ]
+  constructor(private router:Router,
+              private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.req = this.http.get('assets/json/videos.json').subscribe(data=>{
+      console.log(data);
+
+      ((item: any)=>{
+        if (item.featured){
+          console.log(item)
+          this.homeImageList2.push(item);
+        }
+      })
+      // this.homeImageList2 = data as [];
+          })
+  }
+
+
+  ngOnDestroy(): void {
+    this.req.unsubscribe()
   }
 
   preventNormal(event:MouseEvent, image:any){
